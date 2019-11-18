2c013de064c9fd4a2eb6a9be1fb03d154fbb26b9
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2019-11-18 22:15:25 +0530

    change image asset

--- pixel-sorting.js
+++ pixel-sorting.js
@@ -1,27 +1,51 @@
+/**
+ * A Canvas2D example of async loading and image processing.
+ * @author Matt DesLauriers (@mattdesl)
+ */
+
 const canvasSketch = require('canvas-sketch');
 const load = require('load-asset');
 
-// We create an 'async' sketch
 canvasSketch(async ({ update }) => {
-  // Await the image loader, it returns the loaded <img>
-  const image = await load('assets/baboon.jpg');
+  const image = await load('assets/38975782.jpeg');
 
-  // Once the image is loaded, we can update the output
-  // settings to match it
+  // Update our sketch with new settings
   update({
     dimensions: [ image.width, image.height ]
   });
 
-  // Now render our sketch
+  // Render our sketch
   return ({ context, width, height }) => {
-    // Draw the loaded image to the canvas
+    // Render to canvas
     context.drawImage(image, 0, 0, width, height);
 
     // Extract bitmap pixel data
     const pixels = context.getImageData(0, 0, width, height);
 
-    // Manipulate pixel data
-    // ... sort & glitch pixels ...
+    // Manipulate pixels
+    const data = pixels.data;
+    let len = width;
+    while (len) {
+      const newX = Math.floor(Math.random() * len--);
+      const oldX = len;
+
+      // Sometimes leave row in tact
+      if (Math.random() > 0.85) continue;
+
+      for (let y = 0; y < height; y++) {
+        // Sometimes leave column in tact
+        if (Math.random() > 0.925) continue;
+
+        // Copy new random column into old column
+        const newIndex = newX + y * width;
+        const oldIndex = oldX + y * width;
+
+        // Make 'grayscale' by just copying blue channel
+        data[oldIndex * 4 + 0] = data[newIndex * 4 + 2];
+        data[oldIndex * 4 + 1] = data[newIndex * 4 + 2];
+        data[oldIndex * 4 + 2] = data[newIndex * 4 + 2];
+      }
+    }
 
     // Put new pixels back into canvas
     context.putImageData(pixels, 0, 0);
